71c3a2d57e1614c9129a432dd7f03677a133e8ef
Evan Felix <evan.felix@pnnl.gov>
2015-07-21 10:59:48 -0700

    move some code about, add tests, scaffold out some functions

--- BitSet.cpp
+++ BitSet.cpp
@@ -1,10 +1,21 @@
 #include "BitSet.h"
 
 
-BitSet::BitSet() {
+BitSet::BitSet(int size) {
 	
 }
 
 BitSet::~BitSet() {
+	
+}
+
+BitSet BitSet::Union(const BitSet &other) {
+
+}
+void BitSet::Set(long member) {
+
+}
+
+BitSet BitSet::operator=(const BitSet &other) {
 	
 }
--- BitSet.h
+++ BitSet.h
@@ -2,24 +2,24 @@
 
 class BitSet {
 public:
+	BitSet(int size=64);
+	~BitSet();
 	BitSet Union(const BitSet &other);
 	BitSet Intersect(const BitSet &other);
-	Set(long member);
-	IsMember(long member);
-	Invert();
+	void Set(long member);
+	void IsMember(long member);
+	void Invert();
 	BitSet Inverted();
 	int Count();
 	BitSet operator=(const BitSet &other);	
 	BitSet operator|(const BitSet &other);
-	operator|=(const BitSet &other);
+	void operator|=(const BitSet &other);
 	BitSet operator&(const BitSet &other);
-	operator&=(const BitSet &other);
+	void operator&=(const BitSet &other);
 	BitSet operator~();
 private:
 	uint64_t *bits;
-}
-
-
+};
 
 /* sample code 
 
